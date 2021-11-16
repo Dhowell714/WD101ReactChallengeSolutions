d0202fbfb7509242734a4af1b8e0b8d1ade7d434
KateLockhart <katelockhart707@gmail.com>
2021-11-16 17:07:16 -0500

    Finished Inputs.js file logic and build with notes.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+// Importing our css stylesheet
 import './App.css';
+// Importing Bootstrap's styling library
+import 'bootstrap/dist/css/bootstrap.css';
+// Importing useState into the project from React so it can be used to set and pass state variables
+import { useState } from 'react';
 
 function App() {
+
+  // [1] First instruction after creating app is to: In App.js, set up 3 state variables. One for name, one for age, and one for hobbies.
+
+  // Declaring and constructing the three state variables with the proper syntax as instructed:
+  const [ name, setName ] = useState('');
+  const [ age, setAge ] = useState('');
+  const [hobbies, setHobbies ] = useState('');
+
+  // [2] The next instruction asks us to create our components folder with our Inputs.js file and View.js file.
+  // They each have three steps to complete before being imported here to access and use them.
+
   return (
     <div className="App">
+      <h1>React Challenge One:</h1>
       
     </div>
   );
---
+++ src/components/Inputs.js
@@ -0,0 +1,61 @@
+// For this form, I have imported Reactstrap to demonstrate utilizing their library 
+import { Form, FormGroup, Label, Input } from 'reactstrap';
+
+// Declare our functional component with name that matches our file title, pass in properties shared from parent file of App.js using the parameter name of 'props' 
+// (1) This satisfies the first responsibility of Input.js needing to accept props
+const Inputs = ( props ) => {
+
+    return (
+    <div>
+        {/* (2) The second responsibility of Input.js is to have three separate inputs: one for name, one for age, and one for hobbies
+        I imported a Form from Reactstrap to quickly grab something visually pleasing */}
+        <Form>
+
+            {/* (2.1) This FormGroup will hold my first input for our name property. */}
+            <FormGroup>
+                <Label for='name'>Name:</Label>
+                <Input
+                    type='text'
+                    name='name'
+                    id='nameInput'
+                    placeholder="Please enter your name"
+                    // (3) Each input needs it's own means to update the state
+                    // Here we can use an onChange to pass any changes in value via the input and set our state.
+                    // We are diving into our props, choosing which value we want to update, and assigning it to the input's value.
+                    // Here we are changing the value for name with an onChange reassigning setName from our passed props
+                    onChange={(e) => props.setName(e.target.value)}
+                />
+            </FormGroup>
+
+            {/* (2.2) This FormGroup will hold my second input for our age property. */}
+            <FormGroup>
+                <Label for='age'>Age:</Label>
+                <Input
+                    type='number'
+                    name='age'
+                    id='ageInput'
+                    placeholder="Please enter your age"
+                    // Here we are changing the value for age with an onChange reassigning setAge from our passed props 
+                    onChange={(e) => props.setAge(e.target.value)}
+                />
+            </FormGroup>
+
+            {/* (2.3) This FormGroup will hold my third input for our hobbies property. */}
+            <FormGroup>
+                <Label for='hobbies'>Hobbies:</Label>
+                <Input
+                    type='textarea'
+                    name='hobbies'
+                    id='hobbiesInput'
+                    placeholder="Please list your hobbies"
+                    // Here we are changing the value for hobbies with an onChange reassigning setHobbies from our passed props
+                    onChange={(e) => props.setHobbies(e.target.value)}
+                />
+            </FormGroup>
+        </Form>
+    </div>    
+    );
+};
+
+// Exporting the functional component so it can be utilized wherever it's imported
+export default Inputs;
